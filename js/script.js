c1257b093ee0867c7ba2f909bf42864c316fb8ff
Luis Waderdorff <walderdorff@live.de>
2018-10-26 15:55:44 +0200

    A lot of changes

--- js/script.js
+++ js/script.js
@@ -9,35 +9,35 @@ $scrollLink.click(function(event) {
   }, 1000 )
 })
 
-// Header sticky on scroll
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {stickyFunction()};
+// // Header sticky on scroll
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {stickyFunction()};
+//
+// // Get the header
+// var header = document.querySelector(".main-nav");
+//
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
 
-// Get the header
-var header = document.querySelector(".main-nav");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyFunction() {
-  if (window.pageYOffset > sticky && $(window).width() >= 768) {
-    header.classList.add("sticky");
-  } else if (window.pageYOffset <= sticky || $(window).width() < 768) {
-    header.classList.remove("sticky");
-  }
-}
-
-// Active link switching
-$(window).scroll(function() {
-  const scrollbarLocation = $(this).scrollTop();
-
-  $scrollLink.each(function() {
-    const $sectionOffset = $(this.hash).offset().top;
-
-    if ($sectionOffset <= scrollbarLocation) {
-      $(this).parent().siblings().children().removeClass("active");
-      $(this).addClass("active");
-    }
-  })
-})
+  // // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  // function stickyFunction() {
+  //   if (window.pageYOffset > sticky && $(window).width() >= 768) {
+  //     header.classList.add("sticky");
+  //   } else if (window.pageYOffset <= sticky || $(window).width() < 768) {
+  //     header.classList.remove("sticky");
+  //   }
+  // }
+  //
+  // // Active link switching
+  // $(window).scroll(function() {
+  //   const scrollbarLocation = $(this).scrollTop();
+  //
+  //   $scrollLink.each(function() {
+  //     const $sectionOffset = $(this.hash).offset().top;
+  //
+  //     if ($sectionOffset <= scrollbarLocation) {
+  //       $(this).parent().siblings().children().removeClass("active");
+  //       $(this).addClass("active");
+  //     }
+  //   })
+  // })
