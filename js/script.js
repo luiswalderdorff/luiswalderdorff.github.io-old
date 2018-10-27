cdd8127fc687b285d3c115bdef52bbef169d3338
Luis Waderdorff <walderdorff@live.de>
2018-10-27 14:26:17 +0200

    Mostly finished

--- js/script.js
+++ js/script.js
@@ -6,38 +6,31 @@ $scrollLink.click(function(event) {
   event.preventDefault();
   $("body,html").animate({
     scrollTop: $(this.hash).offset().top
-  }, 1000 )
-})
+  }, 1000 );
+});
 
-// // Header sticky on scroll
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {stickyFunction()};
-//
-// // Get the header
-// var header = document.querySelector(".main-nav");
-//
-// // Get the offset position of the navbar
-// var sticky = header.offsetTop;
+// searchBox
 
-  // // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  // function stickyFunction() {
-  //   if (window.pageYOffset > sticky && $(window).width() >= 768) {
-  //     header.classList.add("sticky");
-  //   } else if (window.pageYOffset <= sticky || $(window).width() < 768) {
-  //     header.classList.remove("sticky");
-  //   }
-  // }
-  //
-  // // Active link switching
-  // $(window).scroll(function() {
-  //   const scrollbarLocation = $(this).scrollTop();
-  //
-  //   $scrollLink.each(function() {
-  //     const $sectionOffset = $(this.hash).offset().top;
-  //
-  //     if ($sectionOffset <= scrollbarLocation) {
-  //       $(this).parent().siblings().children().removeClass("active");
-  //       $(this).addClass("active");
-  //     }
-  //   })
-  // })
+function searchBox () {
+	// Declare variables
+	var input, filter, users, user, picture, i;
+	input = document.getElementById('search');
+	filter = input.value.toUpperCase();
+	users = $(".projects");
+	user = $(".projects div");
+
+	// Loop through all user(s) and hide those who dont match the search query
+	for (i = 0; i < user.length; i++) {
+    const boxText0 = user[i].getElementsByTagName("p")[0];
+		const boxText1 = user[i].getElementsByTagName("p")[1];
+		if (boxText1) {
+			if (boxText1.innerHTML.toUpperCase().indexOf(filter) > -1) {
+				user[i].style.display = "";
+			} else if (boxText0.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        user[i].style.display = "";
+      } else {
+				user[i].style.display = "none";
+			}
+		}
+  }
+}
